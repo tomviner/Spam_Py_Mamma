c3731d916f701556b4a10ce319d532e6379aa744
Gump <michal@michal.tv>
2012-03-01 21:21:11 +0000

    Changed api

--- replier.py
+++ replier.py
@@ -1,11 +1,15 @@
 from romannums import attempt_roman 
 
+def hashtag(f):
+    def inner(self, *args, **kwargs):
+        return '@' + args[0] + ' ' + f(self, *args[1:], **kwargs) + " #ldnpydojo"
+    return inner
 
 class Replier(object):
     
     replies = {
         'mum': "That's what your mum said",    
-        'anywhere': 'PythonAnywhere DOT com',
+        'anywhere': 'You must check out PythonAnywhere.com',
     }
 
 
@@ -17,10 +21,11 @@ class Replier(object):
         'beer': replies['mum'],
     }
 
+    @hashtag
     def analyse_tweet(self, tweet):
         roman_result = attempt_roman(tweet)
         if roman_result:
-            return roman_result
+            return "Mama says romans count it as: %d" % roman_result
 
         for keyword, reply in self.keywords.items():
             if keyword in tweet:
@@ -28,6 +33,6 @@ class Replier(object):
         return "Spammama doesn't know"
 
 replier = Replier()
-print replier.analyse_tweet('I got hammered today');
-print replier.analyse_tweet('Fancy a meal somwhere?');
-print replier.analyse_tweet('MMCXL');
+print replier.analyse_tweet('test', 'I got hammered today');
+print replier.analyse_tweet('gump', 'Fancy a meal somwhere?');
+print replier.analyse_tweet('Anthony', 'MMCXL');
